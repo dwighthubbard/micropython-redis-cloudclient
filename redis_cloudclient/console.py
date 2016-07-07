dc408db97f3e170470ed2f6afc6bfb2843ee644b
Dwight Hubbard <d@d-h.us>
2016-07-07 10:40:09 -0700

    Code cleanups/refactoring
    Abstract out the handling of handlers

--- redis_cloudclient/console.py
+++ redis_cloudclient/console.py
@@ -8,12 +8,10 @@ class RedisStream(object):
     File I/O object that streams data to/from redis keys (strings)
 
     """
-    redis_key = b'RedisRepl'
-    buffersize = 80  # Size of the buffer in bytes
     _read_position = 0
     _connection = None
 
-    def __init__(self, redis, redis_key, buffer_size=256):
+    def __init__(self, redis, redis_key, buffer_size=80):
         self._connection = redis
         self.redis = redis
         self.redis_key = redis_key
--- redis_cloudclient/eventloop.py
+++ redis_cloudclient/eventloop.py
@@ -9,37 +9,28 @@ class EventLoop(object):
     """
     Main eventloop object to handle various events on the device
     """
-
-    def __init__(self, redis_server=None, redis_port=18266, enable_logging=False):
+    handlers = {
+        'exec': 'exec_command',
+        'copy': 'copy_file'
+    }
+    def __init__(self, name=None, redis_server=None, redis_port=18266, enable_logging=False):
         self.enable_logging = enable_logging
+        self.name = name
+        self.redis_server = redis_server
+        self.redis_port = redis_port
 
-        if not redis_server:
-            from bootconfig.config import get
-            redis_server = get('redis_server')
-            redis_port = get('redis_port')
-            if redis_port:
-                redis_port = int(redis_port)
-        print('Connecting to Redis server at %s:%d' % (redis_server, redis_port))
-        self.redis_connection = Client(redis_server, redis_port)
+        self._get_redis_host_and_port()
+        print('Connecting to Redis server at %s:%d' % (self.redis_server, self.redis_port))
+        self.redis_connection = Client(self.redis_server, self.redis_port)
 
-        self.name = get('name')
-        self.base_key = 'repl:' + self.name
-        self.command_key = self.base_key + '.command'
-        self.console_key = self.base_key + '.console'
-        self.complete_key = self.base_key + '.complete'
-        if self.enable_logging:
-            from .logging import Logger
-            self.logger = Logger(self.redis_connection, self.name)
+        self._determine_keys()
+        self._enable_logging()
+        self._find_handlers()
+        self._initialize_console()
 
-        from .console import RedisStream
-        self.console = RedisStream(redis=self.redis_connection, redis_key=self.console_key)
-        if sys.platform not in ['WiPya']:
-            from uos import dupterm
-            dupterm(self.console)
-        self.clear_keys()
-        self.console.clear()
-
-
+    ################################################################
+    # Init operations
+    ################################################################
     def clear_keys(self):
         """
         Clear the redis keys for this eventloop
@@ -47,35 +38,64 @@ class EventLoop(object):
         for key in [self.complete_key, self.console_key]:
             self.redis_connection.execute_command('DEL', key)
 
-    def exec_command(self, command):
+    def _determine_keys(self):
         """
-        Execute a single command.
-
-        Parameters
-        ----------
-        command: bytes
-            The command to execute
-
-        Returns
-        -------
-        int
-            The return code of the command, will be 0 if the command completed
-            sucessfully and 1 if it generated an exception.
+        Calculate the redis key names to use for different operations.
         """
+        if not self.name:
+            from bootconfig.config import get
+            self.name = get('name')
+        self.base_key = 'repl:' + self.name
+        self.command_key = self.base_key + '.command'
+        self.console_key = self.base_key + '.console'
+        self.complete_key = self.base_key + '.complete'
+
+    def _enable_logging(self):
+        """
+        Create a Logger object to send logs to redis if logging is enabled.
+        """
+        if self.enable_logging:
+            from .logging import Logger
+            self.logger = Logger(self.redis_connection, self.name)
+
+    def _find_handlers(self):
+        """
+        Iterate the handlers dictionary and replace string handler names with
+        the method
+        """
+        for key in self.handlers.keys():
+            if isinstance(self.handlers[key], str):
+                try:
+                    operation = getattr(self, key)
+                except AttributeError:
+                    # No method with the specified name
+                    continue
+                self.handlers[key] = operation
+
+    def _get_redis_host_and_port(self):
+        """
+        Determine the redis server host and port values, getting them from the
+        bootconfig configuration if they aren't set.
+        """
+        if not self.redis_server:
+            from bootconfig.config import get
+            self.redis_server = get('redis_server')
+            redis_port = get('redis_port')
+            if redis_port:
+                self.redis_port = int(redis_port)
+
+    def _initialize_console(self):
+        """
+        Initialize the console redirection for the event loop
+        """
+        from .console import RedisStream
+        self.console = RedisStream(redis=self.redis_connection, redis_key=self.console_key)
+        if sys.platform not in ['WiPy']:
+            # Dupterm is currently broken on wipy
+            from uos import dupterm
+            dupterm(self.console)
+        self.clear_keys()
         self.console.clear()
-        self.clear_completion_queue()
-        self.heartbeat(state=b'running', ttl=30)
-        try:
-            exec(command)
-            rc = 0
-        except Exception as exc:
-            from sys import print_exception
-            print_exception(exc)
-            rc = 1
-        self.console.flush()
-        self.signal_completion(rc)
-        self.heartbeat(state=b'idle')
-        return rc
 
     def clear_completion_queue(self):
         self.redis_connection.execute_command('DEL', self.complete_key)
@@ -124,7 +144,6 @@ class EventLoop(object):
         if command:
             return command[1]
 
-    # Operations handlers
     def handle_command(self):
         """
         Check for and execute commands from the command queue
@@ -144,6 +163,38 @@ class EventLoop(object):
         while True:
             self.heartbeat(state=b'idle')
             self.handle_command()
+
+    # Operations handlers
+    def exec_command(self, command):
+        """
+        Execute a single command.
+
+        Parameters
+        ----------
+        command: bytes
+            The command to execute
+
+        Returns
+        -------
+        int
+            The return code of the command, will be 0 if the command completed
+            sucessfully and 1 if it generated an exception.
+        """
+        self.console.clear()
+        self.clear_completion_queue()
+        self.heartbeat(state=b'running', ttl=30)
+        try:
+            exec(command)
+            rc = 0
+        except Exception as exc:
+            from sys import print_exception
+            print_exception(exc)
+            rc = 1
+        self.console.flush()
+        self.signal_completion(rc)
+        self.heartbeat(state=b'idle')
+        return rc
+
 
 def start():
     """

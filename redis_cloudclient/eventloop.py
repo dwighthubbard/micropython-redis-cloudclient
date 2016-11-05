ecf32cf9f6ba54edb599efebbc85d2c2d9ff9805
Dwight Hubbard <d@dhub.me>
2016-11-04 23:17:33 -0700

    Use bytestrings

--- redis_cloudclient/eventloop.py
+++ redis_cloudclient/eventloop.py
@@ -78,20 +78,20 @@ class EventLoop(object):
         the method
         """
         for key, value in self.handlers.items():
-            self.logger.debug('1')
+            self.logger.debug('1', key)
             if isinstance(self.handlers[key], str):
-                self.logger.debug('2')
+                self.logger.debug('2', key)
                 try:
                     operation = getattr(self, self.handlers[key])
                 except AttributeError:
                     # No method with the specified name
                     print('No method %r found' % self.handlers[key])
                     continue
-                self.logger.debug('3')
+                self.logger.debug('3', key)
                 del self.handlers[key]
-                self.logger.debug('4')
+                self.logger.debug('4', key)
                 new_key = self.base_key + b'.' + key.encode()
-                self.logger.debug('5')
+                self.logger.debug('5', key)
                 self.handlers[new_key] = operation
 
     def _get_redis_host_and_port(self):
